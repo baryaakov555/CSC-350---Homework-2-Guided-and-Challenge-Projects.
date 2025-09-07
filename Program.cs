bf438b9b276b02b46278ba7fcd2980d961aa4a36
Bar Yaakov <baryaakov5050@gmail.com>
2025-09-07 12:03:04 -0400

    Added the letter grades that are calculated based on the average grade of the 5 assignment scores

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 int[] studentScores = new int[10];
+string currectStudentLetterGrade = "";
 
 foreach (string name in studentNames)
 {
@@ -32,6 +33,33 @@ foreach (string name in studentNames)
     }
 
     currentStudentGrade = (decimal)sumOfAssignmentScores / currentAssignments;
+
+    if (currentStudentGrade >= 97)
+        currectStudentLetterGrade = "A+";
+    else if (currentStudentGrade >= 93)
+        currectStudentLetterGrade = "A";
+    else if (currentStudentGrade >= 90)
+        currectStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 87)
+        currectStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 83)
+        currectStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 80)
+        currectStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 77)
+        currectStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 73)
+        currectStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 70)
+        currectStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 67)
+        currectStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 63)
+        currectStudentLetterGrade = "D";
+    else if (currentStudentGrade >= 60)
+        currectStudentLetterGrade = "D-";
+    else
+        currectStudentLetterGrade = "F";
 
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
