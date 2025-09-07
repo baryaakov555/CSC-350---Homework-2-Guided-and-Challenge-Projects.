5667f14d37448462e87b03b87dc9d69c8249a042
Bar Yaakov <baryaakov5050@gmail.com>
2025-09-07 12:48:22 -0400

    Changed variable names so that we can calculate the sum and average of all the students and not just Sophia, and generalized the calculations so they fit each case or each student"

--- Program.cs
+++ Program.cs
@@ -8,25 +8,32 @@ int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+int[] studentScores = new int[10];
 
 foreach (string name in studentNames)
 {
-    if (name == "Sophia")
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emaa")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumOfAssignmentScores = 0;
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
     {
-        int sophiaSum = 0;
-        decimal sophiaScore;
-
-        foreach (int score in sophiaScores)
-        {
-            sophiaSum += score;
-        }
-
-        sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-        Console.WriteLine("Student\t\tGrade\n");
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        sumOfAssignmentScores += score;
     }
 
+    currentStudentGrade = (decimal)sumOfAssignmentScores / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
 }
 
 Console.WriteLine("Press the Enter key to continue");
